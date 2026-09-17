226f04f1fdc8da5b5756d93c665073ced113e3e2
Joaolucas2007 <joao.git2000p@gmail.com>
2026-09-17 19:06:56 -0300

    Feat: Criando e testando getters e setters

--- projetostreaming/TestaUsuario.java
+++ projetostreaming/TestaUsuario.java
@@ -13,10 +13,42 @@ public class TestaUsuario {
 
         //Agora usando o construtor para inserir os dados do usuario
         Usuario usu1 = new Usuario("João Silva", "joao.silva@gmail.com", "Premium");
+        Usuario usu2 = new Usuario("Maria Souza", "maria.souza@gmail.com", "Básico");
 
-    
+        //Chamando os metodos da classe Usuario
         usu1.assistir();   
-        
         usu1.exibirInformacoes();
+
+        usu2.assistir();
+        usu2.exibirInformacoes();
+        
+        //Testando o metodo getters trazendo os dados do usuario 1 e usuario 2
+        System.out.println("Nome do usuário 1: " + usu1.getNome());
+        System.out.println("Email do usuário 1: " + usu1.getEmail());
+        System.out.println("Plano do usuário 1: " + usu1.getPlano());
+
+        System.out.println("Nome do usuário 2: " + usu2.getNome());
+        System.out.println("Email do usuário 2: " + usu2.getEmail());
+        System.out.println("Plano do usuário 2: " + usu2.getPlano());
+
+        //Testando o metodo setters alterando os dados do usuario 1 e usuario 2
+        usu1.setNome("João Pedro");
+        usu1.setEmail("joaopedro@gmail.com");
+        usu1.setPlano("Básico");
+
+        usu2.setNome("Eduarda Lima");
+        usu2.setEmail("Eduardalima@gmail.com");
+        usu2.setPlano("Premium");
+
+        // Verificando os dados alterados pelos setters
+        System.out.println("Dados do usuário 1 após alteração:");
+        System.out.println("Nome: " + usu1.getNome());
+        System.out.println("Email: " + usu1.getEmail());
+        System.out.println("Plano: " + usu1.getPlano());
+
+        System.out.println("Dados do usuário 2 após alteração:");
+        System.out.println("Nome: " + usu2.getNome());
+        System.out.println("Email: " + usu2.getEmail());
+        System.out.println("Plano: " + usu2.getPlano());
     }
 }
--- projetostreaming/Usuario.java
+++ projetostreaming/Usuario.java
@@ -3,9 +3,9 @@ package projetostreaming;
 public class Usuario {
     
     // Criando os Atributos da classe Usuario
-    String nome;
-    String email;
-    String plano;
+   private String nome;
+   private String email;
+   private String plano;
     
     // Criando os metodos da classe usuario
     void assistir () {
@@ -19,6 +19,29 @@ public class Usuario {
     public Usuario(String nome, String email, String plano) {
         this.nome = nome;
         this.email = email;
+        this.plano = plano;
+    }
+    //Criando os metodos Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+    public String getPlano() {
+        return plano;
+    }
+    public void setPlano(String plano) {
         this.plano = plano;
     }
 }
